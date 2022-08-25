a00753680d6d63f6af0c37d36d398cb847123686
maish-geek <wallacemaish@gmail.com>
2022-08-25 12:12:50 +0300

    Exercise 1

---
+++ Exercise 1.py
@@ -0,0 +1,19 @@
+
+
+#print("Hello, Wallace Njuguna!")
+
+
+# created Github account and linked with pycharm
+
+#Variables and Interactive Programs
+
+#user = input("Enter your Name:")
+
+#print("Hello," + user + "!")
+
+#from math import pi
+
+#r = float(input ("input the radius of the circle : "))
+
+#print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
