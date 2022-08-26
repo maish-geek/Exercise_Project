424f60af7b5f5f822456b04fcb82dcdf49e19285
maish-geek <wallacemaish@gmail.com>
2022-08-26 10:30:12 +0300

    Lesson 1 Exercises 1,2 & 1,2,3,4,6

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,15 +1,19 @@
-
+#QUESTION 1.1
 
 #print("Hello, Wallace Njuguna!")
 
-
+#QUESTION 1.2
 # created Github account and linked with pycharm
 
 #Variables and Interactive Programs
 
+#QUESTION 2.1
+
 #user = input("Enter your Name:")
 
 #print("Hello," + user + "!")
+
+#QUESTION 2.2
 
 #from math import pi
 
@@ -17,3 +21,40 @@
 
 #print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
 
+# QUESTION 2.3
+
+#l=int(input("Length : "))
+
+#w=int(input("Width : "))
+
+#area=l*w
+
+#perimeter=2*(l+w)
+
+#print("Area of Rectangle : ",area)
+
+#print("Perimeter of Rectangle : ",perimeter)
+
+#QUESTION 2.4
+
+#x = int(input("Please enter the first Value: "))
+#y = int(input("Please enter the second value: "))
+
+#print("sum = {}".format(x+y))
+#print("product = {}".format(x*y))
+#print("average = {}".format((x+y)/2))
+
+#QUESTION 2.5
+
+#QUESTION 2.6
+
+#from random import randint
+#threedigitcode = ""
+#for x in range (3):
+ #   threedigitcode = threedigitcode + str(randint(0,9))
+#print(threedigitcode)
+
+#fourdigitcode = ""
+#for y in range(4):
+ #   fourdigitcode = fourdigitcode + str(randint(1,6))
+#print(fourdigitcode)
