413c27510266bd975c010469efb3de591e3af06a
Wallace Mathenge Njuguna <62329458+maish-geek@users.noreply.github.com>
2022-09-14 09:16:19 +0300

    Update Exercise 1.py

--- Exercise 1.py
+++ Exercise 1.py
@@ -189,3 +189,48 @@ while password!='rules' and username!='python' and count<5:
     else:
         print('Access denied. Try again.')
         count-=1
+
+       
+       import sys
+
+#Question 5.1
+
+import random
+
+
+def main():
+    while True:
+
+        num_dice = int(input('How many dice do you want to roll?'))
+        if num_dice < 1 or num_dice > 10:
+            print('Enter a number between 1 and 10.')
+        else:
+            break
+    result = roll_dice(num_dice)
+    print(f'The total for {num_dice} rolls was {result}.')
+
+
+def roll_dice(num_dice):
+    rolls = 0
+    for i in range(1, num_dice + 1):
+        print(f'Roll #{i} = {(roll := random.randint(1, 6))}')
+        rolls += roll
+    return rolls
+
+
+main()
+
+#Question 5.3
+
+x = input('Enter an integer: ')
+prime = True  # initial set
+
+for i in range(2, x):
+    if x % i == 0:
+        prime = False
+        break
+
+if prime:
+    print(str(x) + ' is prime.')
+else:
+    print(str(x) + ' is not prime')
