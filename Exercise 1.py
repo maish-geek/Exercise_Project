e8589d03a42f3d1fed772ff7aaa271e2c3bd27c3
Wallace Mathenge Njuguna <62329458+maish-geek@users.noreply.github.com>
2022-09-29 08:45:21 +0300

    Update Exercise 1.py

--- Exercise 1.py
+++ Exercise 1.py
@@ -448,5 +448,34 @@ for i in config:
 
 
   
+#Question 8.3
+
+import mysql.connector
+import geopy
+from geopy import distance
+
+conn = mysql.connector.connect(host="localhost",
+                               user="root",
+                               password="NjugunaM@93",
+                               database="flight_game"
+                               )
+
+icao_code1 = input("Enter the first ICAO code")
+icao_code2 = input("Enter the second ICAO code")
+
+sql = "select latitude_deg, longitude_deg from airport where ident = " + "'" + icao_code1 + "'"
+stm = "select latitude_deg, longitude_deg from airport where ident = " + "'" + icao_code2 + "'"
+
+con_fig = conn.cursor()
+con_fig.execute(sql)
+
+for i in con_fig:
+    print(i)
+
+con_fig.execute(stm)
+for j in con_fig:
+    print(j)
+
+print(distance.distance(i, j).km)
   
   
